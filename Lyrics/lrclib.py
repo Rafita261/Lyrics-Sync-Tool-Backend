8216476aed9aff4ba2065fca1d5a7bf6ad4dc5df
Rafita261 <ratsimbazafyf.christalin@gmail.com>
2025-12-27 17:20:12 +0300

    Fix function surcharge

--- Lyrics/lrclib.py
+++ Lyrics/lrclib.py
@@ -1,7 +1,7 @@
 import requests
 
-def get_lyrics_from_lrclib(artiste : str, titre : str) -> str :
-    SEARCH_URL = f"https://lrclib.net//api/get?artist_name={artiste}&track_name={titre}"
+def get_lyrics_from_lrclib(artiste = "" , titre = "", url=None) -> str :
+    SEARCH_URL = url or f"https://lrclib.net//api/get?artist_name={artiste}&track_name={titre}"
 
     data = dict(requests.get(SEARCH_URL).json())
 
@@ -10,13 +10,6 @@ def get_lyrics_from_lrclib(artiste : str, titre : str) -> str :
     
     return data["plainLyrics"]
 
-def get_lyrics_from_lrclib(url : str) -> str :
-    data = dict(requests.get(url).json())
-
-    if "statusCode" in data and data["statusCode"] == 404 :
-        return None
-    
-    return data["plainLyrics"]
 
 if __name__ == '__main__' :
-    print(get_lyrics_from_lrclib("Ceasar","Tanteraky"))
+    print(get_lyrics_from_lrclib(url="https://lrclib.net//api/get?artist_name=Dadju&track_name=Ma+Woman"))
--- Lyrics/ovh.py
+++ Lyrics/ovh.py
@@ -1,7 +1,7 @@
 import requests
 
-def get_lyrics_from_ovh(artiste, titre) :
-    SEARCH_URL = "https://api.lyrics.ovh/v1/"
+def get_lyrics_from_ovh(artiste = "", titre = "", url = None) :
+    SEARCH_URL = url or "https://api.lyrics.ovh/v1/"
     SEARCH_URL+=artiste+"/"+titre
 
     data = dict(requests.get(SEARCH_URL).json())
@@ -9,13 +9,7 @@ def get_lyrics_from_ovh(artiste, titre) :
         return data['lyrics']
     return None
 
-def get_lyrics_from_ovh(url) :
-    data = dict(requests.get(url).json())
-    if 'lyrics' in data :
-        return data['lyrics']
-    return None
-
 # TEST
 
 if __name__ == '__main__' :
-    print(get_lyrics_from_ovh("Rim ka","Vadiko"))
+    print(get_lyrics_from_ovh(url="https://api.lyrics.ovh/v1/Dadju/Reine"))
